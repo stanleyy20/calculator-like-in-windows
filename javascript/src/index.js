ee4ac1f279f6798449dd89083df174ef44745655
Dawid Stach <dstach20@gmail.com>
2022-02-19 17:43:57 +0100

    feat: implementation of basic buttons

--- javascript/src/index.js
+++ javascript/src/index.js
@@ -68,6 +68,16 @@ class Calculator {
     this.bindFunctionToButton(MEMORY_ADD_ID, () => this.memoryAdd());
     this.bindFunctionToButton(MEMORY_MINUS_ID, () => this.memoryMinus());
     this.bindFunctionToButton(MEMORY_SET_ID, () => this.memorySet());
+    this.bindFunctionToButton(CLEAR_ID, () => this.clear());
+    this.bindFunctionToButton(CANCEL_ID, () => this.cancel());
+    this.bindFunctionToButton(ADDITION_ID, () => this.addition());
+    this.bindFunctionToButton(SUBSTRACTION_ID, () => this.substraction());
+    this.bindFunctionToButton(MULTIPLY_ID, () => this.multiplication());
+    this.bindFunctionToButton(DIVIDE_ID, () => this.divide());
+    this.bindFunctionToButton(EQUAL_ID, () => this.equal());
+    this.bindFunctionToButton(BACK_ID, () => this.back());
+    this.bindFunctionToButton(INVERT_ID, () => this.inversion());
+    this.bindFunctionToButton(COMMA_ID, () => this.addComma());
   }
 
   bindFunctionToButton(id, callback) {
@@ -104,8 +114,7 @@ class Calculator {
 
   memoryRead() {
     this.wasSpecialFunctionClicked = true;
-    this.displayValue = this.memoryValue;
-    this.display.textContent = this.memoryValue.toString();
+    this.changeDisplayValue(this.memoryValue);
   }
 
   memoryAdd() {
@@ -121,6 +130,153 @@ class Calculator {
   memorySet() {
     this.wasSpecialFunctionClicked = true;
     this.memoryValue = Number(this.displayValue);
+  }
+
+  clear() {
+    this.previousValue = null;
+    this.selectedFunction = null;
+    this.changeDisplayValue(null);
+    this.memoryValue = 0;
+  }
+
+  cancel() {
+    this.changeDisplayValue(null);
+  }
+
+  addition(hasRepeatedValue) {
+    this.callPreviousFunctionAndAssignNew(this.addition, hasRepeatedValue);
+    if (this.isFunctionDone) {
+      this.setValuesForIsFunctionDone();
+
+      return;
+    }
+
+    const [displayValue, previousValue] = this.getDisplayAndPreviousValue(hasRepeatedValue);
+    const newValue = displayValue + previousValue;
+
+    this.getRepeatedValue(hasRepeatedValue, newValue);
+    this.setValuesAfterSettingNewValue(newValue);
+  }
+
+  substraction(hasRepeatedValue) {
+    this.callPreviousFunctionAndAssignNew(this.substraction, hasRepeatedValue);
+    if (this.isFunctionDone) {
+      this.setValuesForIsFunctionDone();
+
+      return;
+    }
+
+    const [displayValue, previousValue] = this.getDisplayAndPreviousValue(hasRepeatedValue);
+    let newValue;
+
+    if (this.previousValue !== null) {
+      newValue = hasRepeatedValue ? displayValue - this.repeatedValue : previousValue - displayValue;
+
+      this.getRepeatedValue(hasRepeatedValue, newValue);
+    }
+    this.setValuesAfterSettingNewValue(newValue);
+  }
+
+  multiplication(hasRepeatedValue) {
+    this.callPreviousFunctionAndAssignNew(this.multiplication, hasRepeatedValue);
+    if (this.isFunctionDone) {
+      this.setValuesForIsFunctionDone();
+
+      return;
+    }
+
+    const [displayValue, previousValue] = this.getDisplayAndPreviousValue(hasRepeatedValue);
+    const newValue = displayValue * previousValue;
+
+    this.getRepeatedValue(hasRepeatedValue, newValue);
+    this.setValuesAfterSettingNewValue(newValue);
+  }
+
+  divide(hasRepeatedValue) {
+    this.callPreviousFunctionAndAssignNew(this.divide, hasRepeatedValue);
+    if (this.isFunctionDone) {
+      this.setValuesForIsFunctionDone();
+
+      return;
+    }
+
+    const [displayValue, previousValue] = this.getDisplayAndPreviousValue(hasRepeatedValue);
+    const newValue = hasRepeatedValue
+      ? displayValue / this.repeatedValue
+      : previousValue === 0
+      ? displayValue
+      : previousValue / displayValue;
+
+    this.getRepeatedValue(hasRepeatedValue, newValue);
+    this.setValuesAfterSettingNewValue(newValue);
+  }
+
+  equal() {
+    this.isFunctionDone = false;
+    if (!this.wasEqualClicked) {
+      this.selectedFunction(false);
+    } else {
+      this.selectedFunction(true);
+    }
+
+    this.wasEqualClicked = true;
+  }
+
+  inversion() {
+    this.changeDisplayValue(this.displayValue >= 0 ? -Math.abs(this.displayValue) : Math.abs(this.displayValue));
+  }
+
+  back() {
+    this.changeDisplayValue(this.displayValue ? this.displayValue.slice(0, -1) : null);
+  }
+
+  addComma() {
+    if (!this.display.textContent.includes('.')) {
+      this.changeDisplayValue(`${this.displayValue ? this.displayValue : '0'}.`);
+    }
+  }
+
+  callPreviousFunctionAndAssignNew(currentFunction, hasRepeatedValue) {
+    if (this.selectedFunction !== currentFunction && this.selectedFunction) {
+      this.selectedFunction(hasRepeatedValue);
+    }
+    this.selectedFunction = currentFunction;
+  }
+
+  setValuesForIsFunctionDone() {
+    this.repeatedValue = Number(this.previousValue);
+    this.displayValue = 0;
+    this.wasEqualClicked = false;
+  }
+
+  getDisplayAndPreviousValue(hasRepeatedValue) {
+    const displayValue = Number(this.display.textContent);
+    const previousValue = hasRepeatedValue ? this.repeatedValue : Number(this.previousValue);
+
+    return [displayValue, previousValue];
+  }
+
+  getRepeatedValue(hasRepeatedValue, newValue) {
+    this.repeatedValue = hasRepeatedValue
+      ? this.repeatedValue
+      : this.wasEqualClicked
+      ? newValue
+      : Number(this.display.textContent);
+  }
+
+  setValuesAfterSettingNewValue(newValue) {
+    this.isFunctionDone = true;
+    this.wasEqualClicked = false;
+    this.displayValue = null;
+    this.display.textContent = this.previousValue !== null ? newValue : this.display.textContent;
+    this.previousValue = this.previousValue !== null ? newValue : this.display.textContent;
+  }
+
+  changeDisplayValue(value) {
+    const isNoValue = value === null || value === '';
+
+    this.displayValue = value;
+    this.display.textContent = isNoValue ? '0' : value.toString();
   }
 }
 
