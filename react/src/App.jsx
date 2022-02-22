4c4afb59221a5d856fb3bcef4b8083e2093eac4f
Dawid Stach <dstach20@gmail.com>
2022-02-22 17:12:25 +0100

    fix

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { default as bemCssModules } from 'bem-css-modules';
 import { default as AppStyles } from './App.module.scss';
 
